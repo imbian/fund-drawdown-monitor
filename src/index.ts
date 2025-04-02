b9a5fbfa7fe0d7c0735e98df0018646a343a4b58
imbian <imbliang@gmail.com>
2025-04-02 19:35:39 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,79 @@
 import { renderHtml } from "./renderHtml";
+import { Router } from 'itty-router'
+const router = Router()
+/*
+Our index route, a simple hello world.
+*/
+router.get("/", () => {
+  return new Response("Hello, world! This is the root page of your Worker template.")
+})
+
+/*
+This route demonstrates path parameters, allowing you to extract fragments from the request
+URL.
+
+Try visit /example/hello and see the response.
+*/
+router.get("/example/:text", ({ params }) => {
+  // Decode text like "Hello%20world" into "Hello world"
+  let input = decodeURIComponent(params.text)
+
+  // Construct a buffer from our input
+  let buffer = Buffer.from(input, "utf8")
+
+  // Serialise the buffer into a base64 string
+  let base64 = buffer.toString("base64")
+
+  // Return the HTML with the string to the client
+  return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
+})
+
+/*
+This shows a different HTTP method, a POST.
+
+Try send a POST request using curl or another tool.
+
+Try the below curl command to send JSON:
+
+$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
+*/
+router.post("/post", async request => {
+  // Create a base object with some fields.
+  let fields = {
+    "asn": request.cf.asn,
+    "colo": request.cf.colo
+  }
+
+  // If the POST data is JSON then attach it to our response.
+  if (request.headers.get("Content-Type") === "application/json") {
+    fields["json"] = await request.json()
+  }
+
+  // Serialise the JSON to a string.
+  const returnData = JSON.stringify(fields, null, 2);
+
+  return new Response(returnData, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+})
+
+/*
+This is the last route we define, it will match anything that hasn't hit a route we've defined
+above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
+
+Visit any page that doesn't exist (e.g. /foobar) to see it in action.
+*/
+router.all("*", () => new Response("404, not found!", { status: 404 }))
+
 
 export default {
-  async fetch(request, env) {
+  async fetch_(request, env) {
     const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
     const { results } = await stmt.all();
 
@@ -10,5 +82,8 @@ export default {
         "content-type": "text/html",
       },
     });
+  },
+  async fetch(request, env) {
+    return new Response(router.handle(request));
   },
 } satisfies ExportedHandler<Env>;
